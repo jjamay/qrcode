7b6ad93036c0866cb9ebf146f8abec1326c6faff
Jordan May <jordaanmay@gmail.com>
2017-11-22 13:53:07 -0500

    fixed input layout

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,20 +4,20 @@ import { Col, FormGroup, Label, Input, } from 'reactstrap';
 const InputForm = ({ handleChange, handleDateChange, curData }) => (
   <div>
     <FormGroup row>
-      <Label sm={2} for="name">Medication Name</Label>
-      <Col sm={10}>
+      <Label sm={3} for="name">Medication Name</Label>
+      <Col sm={9}>
         <Input type="text" name="name" id="name" placeholder="e.g. Apixaban" onChange={handleChange} value={curData.name} />
       </Col>
     </FormGroup>
     <FormGroup row>
-      <Label sm={2} for="quantity">Quantity</Label>
-      <Col sm={10}>
+      <Label sm={3} for="quantity">Quantity</Label>
+      <Col sm={9}>
         <Input type="quantity" name="quantity" id="quantity" placeholder="e.g. 10" onChange={handleChange} value={curData.quantity} />
       </Col>
     </FormGroup>
     <FormGroup row>
-      <Label sm={2} for="units">Units</Label>
-      <Col sm={10}>
+      <Label sm={3} for="units">Units</Label>
+      <Col sm={9}>
         <Input type="select" name="units" id="units" onChange={handleChange} value={curData.units} >
           <option>Pills</option>
           <option>Sprays</option>
